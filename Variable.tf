425b2ff60076c1aea7d975e63483c1fd8c99c167
rameshpaul1988 <70790649+rameshpaul1988@users.noreply.github.com>
2025-02-11 19:39:36 +0530

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -1,1 +1,28 @@
+variable "gcp_project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
 
+variable "gcp_region" {
+  description = "The region to deploy Cloud Run"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cloud_run_service_name" {
+  description = "Name of the Cloud Run service"
+  type        = string
+  default     = "my-cloud-run-service"
+}
+
+variable "artifact_registry_name" {
+  description = "Name of the Artifact Registry repository"
+  type        = string
+  default     = "my-repo"
+}
+
+variable "image_tag" {
+  description = "The tag of the container image to deploy"
+  type        = string
+  default     = "latest"
+}
